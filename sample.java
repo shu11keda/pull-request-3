b66d9b8286bee247aa3032067246b55a9948c7d2
*kefGGsZdGJ <ikeda.shui@f-inet.co.jp>
2025-04-04 15:56:20 +0900

    xをzに変更し、}を追加しました。また、printをprintlnと変更しました

--- sample.java
+++ sample.java
@@ -3,11 +3,12 @@ public class sample {
 		int x = 1;
 		int y = 2;
 　　　　　　　　
-                for(int x = 1;x < 5;x++){
+                for(int z = 1;z < 5;z++){
 		if(y % 2 == 0){
-			System.out.print(y - x);
+			System.out.println(y - x);
 		}
-		System.out.print(x + y);
+		System.out.println(x + y);
 		y += 1;
 	}
+}
 }
