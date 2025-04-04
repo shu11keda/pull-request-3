ba3942031a46d107e809de32e0a3822c21e8f83a
*kefGGsZdGJ <ikeda.shui@f-inet.co.jp>
2025-04-04 15:48:55 +0900

    if文とfor文を追加しました

--- sample.java
+++ sample.java
@@ -2,6 +2,12 @@ public class sample {
 	public static void main(String[] args) {
 		int x = 1;
 		int y = 2;
-		System.out.ptint(x + y);
+　　　　　　　　
+                for(int x = 1;x < 5;x++){
+		if(y % 2 == 0){
+			System.out.print(y - x);
+		}
+		System.out.print(x + y);
+		y += 1;
 	}
 }
